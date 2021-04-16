2cb49c43820d2d27b7113491eb858f269ad025d3
GerardCode <39347487+GerardCod@users.noreply.github.com>
2021-04-16 11:19:55 -0500

    [MERGE] Listado de roles finalizado. (#2)
    * [MODIFY] Modificando index handler en RolController
    
    * [MODIFY] Agregando ruta para listar roles en api.php
    
    * [MODIFY] Terminando el listado de roles

--- app/Http/Controllers/RolController.php
+++ app/Http/Controllers/RolController.php
@@ -4,82 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Models\Rol;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class RolController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Regresa la lista de roles guardados en la base de datos.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(): Response
     {
         //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Rol  $rol
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Rol $rol)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Rol  $rol
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Rol $rol)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Rol  $rol
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Rol $rol)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Rol  $rol
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Rol $rol)
-    {
-        //
+        return response(Rol::all(), Response::HTTP_OK);
     }
 }
