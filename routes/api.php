1575df9e07f5e24e4e2ebd7cecdd2d6bc1b0aec5
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-04-16 11:16:49 -0500

    [MODIFY] Agregando ruta para listar roles en api.php

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\RolController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Crea las rutas para el recurso roles incluye (GET, POST, PUT, DELETE)
+Route::apiResource('roles', RolController::class);
