8afeea72c9cbf1ed50e06d29b8c51e67011f0a8e
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-04-16 18:51:26 -0500

    [MODIFY] Agregando rutas para laboratorios

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CarreraController;
+use App\Http\Controllers\LaboratorioController;
 use App\Http\Controllers\RolController;
 use App\Http\Controllers\UsuarioController;
 use Illuminate\Support\Facades\Route;
@@ -20,3 +21,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('v1/roles', [RolController::class, 'index']);
 Route::apiResource('v1/carreras', CarreraController::class);
 Route::apiResource('v1/usuarios', UsuarioController::class);
+Route::apiResource('v1/laboratorios', LaboratorioController::class);
