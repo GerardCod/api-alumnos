5bb925899d103bfaf74b742cd9e1312d891f2d96
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-04-16 14:10:12 -0500

    [MODIFY] Función store terminada en UsuarioController

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CrearUsuarioRequest;
+use App\Models\Usuario;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class UsuarioController extends Controller
 {
-    //
-    public function store() {
-
+    /**
+     * Crea un nuevo usuario.
+     * @param CrearUsuarioRequest $request cuerpo de la petición.
+     * @return Response cuerpo de la respuesta.
+     */
+    public function store(CrearUsuarioRequest $request): Response {
+        $validData = $request->validated();
+        Usuario::create($validData);
+        return response($validData, Response::HTTP_CREATED);
     }
 }
