8b6ecbcb47d759a706bb8eb7e665fdf08888f9ed
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-04-16 12:00:37 -0500

    [MODIFY] Función index agregada a CarreraController

--- app/Http/Controllers/CarreraController.php
+++ app/Http/Controllers/CarreraController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\CrearCarrera;
 use App\Models\Carrera;
 use Illuminate\Http\Response;
+use function GuzzleHttp\Promise\all;
 
 class CarreraController extends Controller
 {
@@ -16,5 +17,13 @@ class CarreraController extends Controller
     public function store(CrearCarrera $request): Response {
         $carrera = Carrera::create($request->only('nombre'));
         return response($carrera, Response::HTTP_CREATED);
+    }
+
+    /**
+     * Regresa la lista de carreras almacenadas en la base de datos.
+     * @return Response
+     */
+    public function index(): Response {
+        return response(Carrera::all(), Response::HTTP_OK);
     }
 }
