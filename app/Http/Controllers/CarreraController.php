34c5c1d535f7721840b9cdd35046d5b7fe4b04f8
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-04-16 11:46:55 -0500

    [MODIFY] Metodo store terminado en CarreraController

--- app/Http/Controllers/CarreraController.php
+++ app/Http/Controllers/CarreraController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\CrearCarrera;
+use App\Models\Carrera;
+use Illuminate\Http\Response;
 
 class CarreraController extends Controller
 {
-    //
+    /**
+     * Crea una carrera.
+     * @param CrearCarrera $request
+     * @return Response
+     */
+    public function store(CrearCarrera $request): Response {
+        $carrera = Carrera::create($request->only('nombre'));
+        return response($carrera, Response::HTTP_CREATED);
+    }
 }
