e86a0fce9aea55652dee5c01b691edc79550269c
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-04-17 15:31:08 -0500

    [MODIFY] Terminando el seeder de pc

--- database/seeders/PCSeeder.php
+++ database/seeders/PCSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\PC;
 use Illuminate\Database\Seeder;
 
 class PCSeeder extends Seeder
@@ -13,6 +14,14 @@ class PCSeeder extends Seeder
      */
     public function run()
     {
-        //
+        PC::create(['nombre' => 'Pc1']);
+        PC::create(['nombre' => 'Pc2']);
+        PC::create(['nombre' => 'Pc3']);
+        PC::create(['nombre' => 'Pc4']);
+        PC::create(['nombre' => 'Pc5']);
+        PC::create(['nombre' => 'Pc6']);
+        PC::create(['nombre' => 'Pc7']);
+        PC::create(['nombre' => 'Pc8']);
+        PC::create(['nombre' => 'Pc9']);
     }
 }
