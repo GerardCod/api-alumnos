c1b8a682d3f701702769d90f63048a0b0292fce8
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-04-16 21:53:52 -0500

    [MODIFY] Endpoint de listado de programas terminado

--- app/Http/Controllers/ProgramaController.php
+++ app/Http/Controllers/ProgramaController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\ProgramaRequest;
 use App\Models\Programa;
-use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 class ProgramaController extends Controller
@@ -18,5 +17,13 @@ class ProgramaController extends Controller
         $validData = $request->validated();
         $program = Programa::create($validData);
         return response($program, Response::HTTP_CREATED);
+    }
+
+    /**
+     * Regresa el listado de programas almacenados en la base de datos.
+     * @return Response cuerpo de la respuesta.
+     */
+    public function index(): Response {
+        return response(Programa::all(), Response::HTTP_OK);
     }
 }
