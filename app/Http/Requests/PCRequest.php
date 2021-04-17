de5bfc2c95be7bd21d489beae880098a914dbdd8
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-04-16 20:59:11 -0500

    [MODIFY] Agregando reglas de validación a PCRequest

--- app/Http/Requests/PCRequest.php
+++ app/Http/Requests/PCRequest.php
@@ -13,7 +13,7 @@ class PCRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -25,6 +25,7 @@ class PCRequest extends FormRequest
     {
         return [
             //
+            'nombre' => 'required'
         ];
     }
 }
