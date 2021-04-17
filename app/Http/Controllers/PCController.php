a904c7805b142e79a48f4e0a273912f7637cf5d3
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-04-16 20:59:33 -0500

    [MODIFY] Terminando la función store en PCController

--- app/Http/Controllers/PCController.php
+++ app/Http/Controllers/PCController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\PCRequest;
+use App\Models\PC;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class PCController extends Controller
 {
     //
+    /**
+     * Crea un nuevo PC.
+     * @param PCRequest $request cuerpo de la petición.
+     * @return Response cuerpo de la respuesta.
+     */
+    public function store(PCRequest $request): Response {
+        $validData = $request->validated();
+        $pc = PC::create($validData);
+        return response($pc, Response::HTTP_CREATED);
+    }
 }
