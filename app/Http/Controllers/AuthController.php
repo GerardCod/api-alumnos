f7e76a6b4e32c94131df88f8b7ae42ad10e7c595
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-04-17 11:20:42 -0500

    [MODIFY] Terminando función login en AuthController

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Models\Usuario;
+use Illuminate\Http\Response;
+
+class AuthController extends Controller
+{
+
+    /**
+     * Realiza la autenticación de un usuario.
+     * @param LoginRequest $request cuerpo de la petición.
+     * @return Response cuerpo de la respuesta.
+     */
+    public function login(LoginRequest $request): Response {
+        $data = $request->validated();
+        $user = Usuario::query()->where('matricula', '=', $data['matricula'])->get()[0];
+        $token = $user->createToken('access_token');
+        return response(['access_token' => $token->plainTextToken], Response::HTTP_CREATED);
+    }
+
+}
