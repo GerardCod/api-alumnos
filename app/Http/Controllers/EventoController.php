44ba28c62406d5b6fcb3e6af8031222f4a42c5eb
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-04-17 15:16:07 -0500

    [MODIFY] Terminando el listado de eventos

--- app/Http/Controllers/EventoController.php
+++ app/Http/Controllers/EventoController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Evento;
+use Illuminate\Http\Response;
 
 class EventoController extends Controller
 {
-    //
+    /**
+     * Regresa la lista de eventos almacenados en la base de datos.
+     * @return Response cuerpo de la respuesta.
+     */
+    public function index(): Response
+    {
+        return response(Evento::all(), Response::HTTP_OK);
+    }
 }
