947001aa9277e40df52d2de29a33cc791646b257
GerardCod <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2021-04-17 15:07:28 -0500

    [MODIFY] Agregando los componentes de la entidad Evento

---
+++ app/Models/Evento.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Evento extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ['id'];
+
+    protected $fillable = ['inicio', 'fin', 'fecha', 'pc_id', 'laboratorio_id'];
+
+    /**
+     * Regresa la información de la entidad PC asociada.
+     * @return BelongsTo
+     */
+    public function pc(): BelongsTo {
+        return $this->belongsTo(PC::class);
+    }
+
+    /**
+     * Regresa la información de la entidad Laboratorio asociada.
+     * @return BelongsTo
+     */
+    public function laboratorio(): BelongsTo {
+        return $this->belongsTo(Laboratorio::class);
+    }
+}
---
+++ database/migrations/2021_04_17_195052_create_eventos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('eventos', function (Blueprint $table) {
+            $table->id();
+            $table->time('inicio');
+            $table->time('fin');
+            $table->date('fecha');
+            $table->foreignId('pc_id')->constrained('p_c_s');
+            $table->foreignId('laboratorio_id')->constrained('laboratorios');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('eventos');
+    }
+}
